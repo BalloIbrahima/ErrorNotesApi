a0612bac7a5754a719531c466d90d0c2c99767dd
mttraore <marytra22@gmail.com>
2022-08-29 10:46:20 +0000

    Solution controller

---
+++ src/main/java/com/errornotes/ErrorNotesApi/controllers/SolutionController.java
@@ -0,0 +1,62 @@
+package com.errornotes.ErrorNotesApi.controllers;
+
+import com.errornotes.ErrorNotesApi.models.Probleme;
+import com.errornotes.ErrorNotesApi.models.Role;
+import com.errornotes.ErrorNotesApi.models.Solution;
+import com.errornotes.ErrorNotesApi.models.User;
+import com.errornotes.ErrorNotesApi.services.ProblemeService;
+import com.errornotes.ErrorNotesApi.services.SolutionService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Api(value = "Solution", description = "Pour la gestion d'un solution")
+@RestController
+@RequestMapping("/solution")
+public class SolutionController {
+
+    @Autowired
+    SolutionService solutionService;
+
+    @Autowired
+    ProblemeService problemeService;
+
+    //La fonction de ceation
+    @ApiOperation(value = "Pour la création d'un user")
+    @PostMapping("/create/{idProbleme}")
+    public Object create(@RequestBody Solution solution, @PathVariable("idProbleme") Long idProbleme) {
+
+        Solution s = solutionService.retrouverParProbleme(solution.getProbleme());
+        Probleme probleme = problemeService.retrouverParId(idProbleme);
+
+        if (s == null) {
+            if (probleme != null) {
+                // role.getListUser().add(user);
+                solution.setProbleme(probleme);
+                return solutionService.createSolution(solution);
+            } else {
+                return "Vous essayez d'attribuez un probleme qui n'exsite pas!";
+            }
+        } else {
+            return "Cette solution existe déja!!";
+        }
+
+    }
+
+
+    //La fonction modification
+    @ApiOperation(value = "Update Solution")
+    @PutMapping("/update/{id}")
+    public Solution update(@PathVariable Long id, @RequestBody Solution solution) {
+        return solutionService.modificationSolution(id, solution);
+    }
+
+    @ApiOperation(value = "Supression d'une solution")
+    @DeleteMapping("/delete/{id}")
+    public void delete(@RequestBody Solution solution) {
+        solutionService.deleteSolution(solution);
+    }
+
+
+}
